4ae5484c32842f68e4e31a75fed147ddfaacd639
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-05-10 15:39:23 -0400

    Complete the CLI's actions. Ready for integration tests
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,19 +1,66 @@
 import { IParsedArgs } from 'argv-utils';
 import { encodeError } from 'error-message-utils';
+import { copyFile, isDirectory, writeTextFile } from 'fs-utils-sync';
 import { IEnvironmentName } from './types.js';
 import { ERRORS } from './environment.errors.js';
-import { validateSourcePath } from './environment.validations.js';
+import {
+  ENVIRONMENT_NAMES,
+  buildEnvironmentPath,
+  buildFilePath,
+} from './environment.utils.js';
+import { buildIndex, buildTypes, buildEnvironment } from './environment.templates.js';
+import {
+  validateSourcePath,
+  canEnvironmentBeInitialized,
+  validateEnvironmentName,
+} from './environment.validations.js';
 
 /* ************************************************************************************************
  *                                         IMPLEMENTATION                                         *
  ************************************************************************************************ */
 
-const __init = (srcPath: string): void => {
+/**
+ * Installs an environment by name.
+ * @param srcPath
+ * @param name
+ */
+const __installEnvironment = (srcPath: string, name: IEnvironmentName): void => copyFile(
+  buildFilePath(srcPath, name),
+  buildFilePath(srcPath, 'environment'),
+);
 
+/**
+ * Initializes the environment base directory as well as all the required files. Once done, it
+ * installs the development environment.
+ * @param srcPath
+ */
+const __init = (srcPath: string): void => {
+  // ensure it can be initialized
+  canEnvironmentBeInitialized(buildEnvironmentPath(srcPath));
+
+  // create all the base files and once done, install the development environment
+  writeTextFile(buildFilePath(srcPath, 'index'), buildIndex());
+  writeTextFile(buildFilePath(srcPath, 'types'), buildTypes());
+  ENVIRONMENT_NAMES.forEach((name) => (
+    writeTextFile(buildFilePath(srcPath, name), buildEnvironment(name === 'production'))
+  ));
+  __installEnvironment(srcPath, 'development');
 };
 
+/**
+ * Installs an environment by name - overriding whatever environment was active. Note that if
+ * the environment has not yet been initialized, it does so prior to the installation.
+ * @param srcPath
+ * @param environment
+ * @throws
+ * - INVALID_ENVIRONMENT_NAME: if the provided environment name is not supported
+ */
 const __install = (srcPath: string, environment: IEnvironmentName): void => {
-
+  validateEnvironmentName(environment);
+  if (!isDirectory(buildEnvironmentPath(srcPath))) {
+    __init(srcPath);
+  }
+  __installEnvironment(srcPath, environment);
 };
 
 /**
